c11717f7d159dee1a3ae33791bbd77ddfa9785dc
Dakaedr <evodekel@gmail.com>
2022-02-01 11:54:21 +0100

    Update init.el

--- init.el
+++ init.el
@@ -27,6 +27,9 @@
 
 
 ;;change font & theme
+(use-package all-the-icons
+  :if (display-graphic-p))
+
 (set-face-attribute 'default nil :font "FiraCode NF" :height 120)
 
 (use-package nord-theme)
@@ -38,10 +41,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default)))
+   '("37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default))
+ '(package-selected-packages
+   '(doom-modeline use-package nord-theme ivy command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
